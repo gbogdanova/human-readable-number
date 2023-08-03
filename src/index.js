0c7c95086a14d2375bd39340c9be90087cb64796
Galya Bogdanova <galya_bogdanova@MacBook-Air-Maksim.local>
2023-08-02 17:20:47 -0700

    feat: the solution of the problem

--- src/index.js
+++ src/index.js
@@ -1,3 +1,18 @@
 module.exports = function toReadable (number) {
-  
-}
+  let arr = ['zero', 'one', 'two', 'three', 'four', 'five', 'six', 'seven', 'eight', 'nine', 'ten', 'eleven', 'twelve', 'thirteen', 'fourteen', 'fifteen', 'sixteen', 'seventeen', 'eighteen', 'nineteen'];
+    if (number < 20) {return arr[number];}
+  let arr2 = ['','','twenty', 'thirty', 'forty', 'fifty', 'sixty', 'seventy', 'eighty', 'ninety'];
+    if (number > 19) {
+        let numSPL = number.toString().split('');
+        if (numSPL.length == 2) {
+          if (numSPL[1] == '0') {return arr2[+numSPL[0]];}
+          return (arr2[+numSPL[0]] + ' ' + arr[+numSPL[1]]);
+        }
+        if (numSPL[1] == '0' && numSPL[2] == '0') {return arr[+numSPL[0]] + ' ' + 'hundred'}
+        if (numSPL[1]  == '0') {return arr[+numSPL[0]] + ' ' + 'hundred' + ' ' + arr[+numSPL[2]]}
+        if (numSPL[1] == '1') {return arr[+numSPL[0]] + ' ' + 'hundred' + ' ' + arr[+numSPL[2]+10]}
+        if (numSPL[2] == '0') {return arr[+numSPL[0]] + ' ' + 'hundred' + ' ' + arr2[+numSPL[1]]}
+        return arr[+numSPL[0]] + ' ' + 'hundred' + ' ' + arr2[+numSPL[1]] + ' ' + arr[+numSPL[2]];
+    } 
+  }
+
